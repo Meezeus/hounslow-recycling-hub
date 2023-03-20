6324fbb6bb5976c63488b6b63aa725412aa072c4
Fatlir Topalli <fatlir-t@outlook.com>
2023-03-20 05:00:10 +0000

    further env var  test

--- src/pages/api/[...param].ts
+++ src/pages/api/[...param].ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { JsxEmit } from 'typescript'
 
 const api = "https://f6omof7w1e.execute-api.eu-west-1.amazonaws.com/prod"
 
@@ -7,11 +8,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const category = param!==undefined ? param[0] : ""
   const id = param!==undefined ? param[1] : ""
   const url = `${api}/${category}` + `${(id !== undefined) ? `/${id}` : ""}`
+  const prodsecrets = process.env.secrets
+  const prodkey = prodsecrets !== undefined ? JSON.parse(prodsecrets).FRONTEND_APIKEY : ""
   const headers = {
     "content-type": "application/json",
-    "x-api-key" : `${process.env.FRONTEND_APIKEY}`
+    "x-api-key" : `${(process.env.NODE_ENV === "production") ? prodkey : process.env.FRONTEND_APIKEY}`
   }
-  
+
   if(req.method === "GET" || req.method === "DELETE"){
     const resapi = await fetch(url, {
       method: req.method,
