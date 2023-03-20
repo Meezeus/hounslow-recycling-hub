8d4dbf625e28960aa58c01874b682b557b178211
jianaiasus <k1897735@kcl.ac.uk>
2023-03-20 02:47:25 +0000

    subheading style

--- components/home/Subheading.tsx
+++ components/home/Subheading.tsx
@@ -1,4 +1,5 @@
 import style from "@/styles/home/Subheading.module.css";
+import { useState } from "react";
 
 type SubheadingProps = {
   title: string;
@@ -6,12 +7,24 @@ type SubheadingProps = {
 };
 
 export default function Subheading(props: SubheadingProps) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className={style["subheading-container-wrapper"]} id={props.id}>
-      <hr className={style["subheading-separator"]} />
+    <div
+      className={style["subheading-container-wrapper"]}
+      id={props.id}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className={style["subheading-container"]}>
         <div className={style["subheading-anchors"]}></div>
-        <h3 className={style["subheading-text"]}>{props.title}</h3>
+        <h3
+          className={`${style["subheading-text"]} ${
+            isHovered ? style["subheading-text-hover"] : ""
+          }`}
+        >
+          {props.title}
+        </h3>
         <div className={style["subheading-anchors"]}></div>
       </div>
       <hr className={style["subheading-separator"]} />
