d875b86707564bb30b290f09cfd815b3e45dfb15
Meezeus <kac3131@gmail.com>
2023-03-21 13:24:42 +0000

    Run prettier

--- src/pages/cms.tsx
+++ src/pages/cms.tsx
@@ -3,8 +3,8 @@ import Header from "@/components/cms/Header";
 import CMSTabs from "@/components/cms/CMSTabs";
 import Footer from "@/components/Footer";
 
-import { Amplify } from 'aws-amplify';
-import awsconfig from '../aws-exports';
+import { Amplify } from "aws-amplify";
+import awsconfig from "../aws-exports";
 Amplify.configure(awsconfig);
 import { withAuthenticator } from "@aws-amplify/ui-react";
 
@@ -16,7 +16,7 @@ type Props = {
   facts: Facts[];
 };
 
-export default withAuthenticator(function CMS({signOut, user}, props: Props) {
+export default withAuthenticator(function CMS({ signOut, user }, props: Props) {
   return (
     <>
       <Head>
@@ -29,17 +29,17 @@ export default withAuthenticator(function CMS({signOut, user}, props: Props) {
 
       <Header />
 
-      <UserHeader signOut={signOut} user={user}/>
+      <UserHeader signOut={signOut} user={user} />
 
       <CMSTabs facts={props.facts} />
 
       <Footer />
     </>
   );
-})
+});
 
 export const getServerSideProps = async () => {
-  const resFacts = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/api/facts`)
+  const resFacts = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/api/facts`);
   const facts = await resFacts.json();
 
   return {
