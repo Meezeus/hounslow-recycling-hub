f8b565e7faab06e86e29d49c86fabfdde939473c
Meezeus <kac3131@gmail.com>
2023-03-21 15:58:02 +0000

    Rename CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="page-content">
+    <div className="page">
       <main>
         <Component {...pageProps} />
       </main>
