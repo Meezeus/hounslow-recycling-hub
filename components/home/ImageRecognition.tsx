1b5b0ea2060446a5dd4f8fe292d7cb880c51d0f6
Meezeus <kac3131@gmail.com>
2023-03-22 22:15:42 +0000

    Extract mapping

--- components/home/ImageRecognition.tsx
+++ components/home/ImageRecognition.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
 import style from "@/styles/home/ImageRecognition.module.css";
 import buttonStyle from "@/styles/home/Button.module.css";
+import {
+  labelToFlatServices,
+  labelToHouseServices,
+} from "@/data/ImageRecognitionMapping";
 
 const ML_ENDPOINT =
   "https://ijtiasa89d.execute-api.eu-west-2.amazonaws.com/default/ML_model";
@@ -21,171 +25,8 @@ export default function ImageRecognition(props: ImageRecognitionProps) {
   const formRef = useRef<HTMLFormElement>(null);
 
   const labelsToServices = props.showFlatVersion
-    ? new Map<string, { name: string; id: string }>([
-        [
-          "plastic",
-          {
-            name: "You can use a communal bin with a red 'household plastic packaging' label!",
-            id: "plastic-recycling-service",
-          },
-        ],
-        [
-          "metal",
-          {
-            name: "You can use a communal bin with a grey 'tins, cans & aerosols' label!",
-            id: "metal-recycling-service",
-          },
-        ],
-        [
-          "cardboard",
-          {
-            name: "You can use a communal bin with a blue 'cardboard' label!",
-            id: "card-recycling-service",
-          },
-        ],
-        [
-          "paper",
-          {
-            name: "You can use a communal bin with a blue 'paper' label!",
-            id: "paper-recycling-service",
-          },
-        ],
-        [
-          "glass",
-          {
-            name: "You can use a communal bin with a teal 'mixed glass' label!",
-            id: "glass-recycling-service",
-          },
-        ],
-        [
-          "food",
-          {
-            name: "You can use a communal bin with a green 'food waste' label!",
-            id: "food-waste-recycling-service",
-          },
-        ],
-        [
-          "rubbish",
-          {
-            name: "This item cannot be recycled. Please use a communal bin with a black 'general waste' label!",
-            id: "general-waste-collection-service",
-          },
-        ],
-        [
-          "textile",
-          {
-            name: "You can utilise TRAID charity and Salvation Army Textile Banks to get your textiles recycled!",
-            id: "textiles-recycling",
-          },
-        ],
-        [
-          "small electrical",
-          {
-            name: "You can utilise TRAID charity as an opportunity to get your small electrical appliances recycled!",
-            id: "small-electrical-items-recycling",
-          },
-        ],
-        [
-          "bulky waste",
-          {
-            name: "You can use the bulky waste collection service!",
-            id: "bulky-waste-collection-service",
-          },
-        ],
-        [
-          "clinical waste",
-          {
-            name: "You can use the clinical waste collection service!",
-            id: "clinical-waste-collection-service",
-          },
-        ],
-      ])
-    : new Map<string, { name: string; id: string }>([
-        [
-          "plastic",
-          {
-            name: "You can use the red recycling box!",
-            id: "plastic-and-metal-recycling-service",
-          },
-        ],
-        [
-          "metal",
-          {
-            name: "You can use the red recycling box!",
-            id: "plastic-and-metal-recycling-service",
-          },
-        ],
-        [
-          "cardboard",
-          {
-            name: "You can use the blue recycling box!",
-            id: "paper-and-card-recycling-service",
-          },
-        ],
-        [
-          "paper",
-          {
-            name: "You can use the blue recycling box!",
-            id: "paper-and-card-recycling-service",
-          },
-        ],
-        [
-          "glass",
-          {
-            name: "You can use the green recycling box!",
-            id: "glass-recycling-service",
-          },
-        ],
-        [
-          "food",
-          {
-            name: "You can use the food waste bin!",
-            id: "food-waste-recycling-service",
-          },
-        ],
-        [
-          "rubbish",
-          {
-            name: "This item cannot be recycled. Please use the black wheeled bin!",
-            id: "general-waste-collection-service",
-          },
-        ],
-        [
-          "textile",
-          {
-            name: "You can use a clearly marked plastic bag and put it alongside your recycling boxes!",
-            id: "textile-recycling-service",
-          },
-        ],
-        [
-          "small electrical",
-          {
-            name: "You can use a clearly marked plastic bag and put it alongside your recycling boxes!",
-            id: "small-electrical-items-recycling-service",
-          },
-        ],
-        [
-          "bulky waste",
-          {
-            name: "You can use the bulky waste collection service!",
-            id: "bulky-waste-collection-service",
-          },
-        ],
-        [
-          "clinical waste",
-          {
-            name: "You can use the clinical waste collection service!",
-            id: "clinical-waste-collection-service",
-          },
-        ],
-        [
-          "garden waste",
-          {
-            name: "You can use the brown wheeled bin!",
-            id: "garden-waste-recycling-service",
-          },
-        ],
-      ]);
+    ? labelToFlatServices
+    : labelToHouseServices;
 
   // This hook is called when the page version changes. It resets the image recognition.
   useEffect(() => {
