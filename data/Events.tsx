d85db5bc33cf3180c898a56cff2150564d7440a4
Meezeus <kac3131@gmail.com>
2023-03-24 19:36:43 +0000

    Change date format

--- data/Events.tsx
+++ data/Events.tsx
@@ -12,8 +12,8 @@ export const events = [
   {
     image: "/brentford_recycling_action_group.png",
     title: "Brentford Recycling Action Group Monthly Meeting",
-    startDate: "07/02/2023",
-    endDate: "07/02/2023",
+    startDate: "2023-02-07",
+    endDate: "2023-02-07",
     description:
       "**What:** *BRAG (Brentford Recycling Action Group)* is a non-political community waste action group made up of local Brentford residents. We work to raise awareness of waste issues and to promote activities and facilities to improve the local environment for the benefit of local people. Meet our fun and enthusiastic group and get inspired!  \n&nbsp;  \n**When:** Our *monthly meetings* take place the *first Tuesday of the month*. The meeting *starts at 7pm* and *finishes at 8pm*.  \n&nbsp;  \n**Where:** At the *Residents Clubroom, Brentford Dock, 1 Augustus Close, Brentford, TW8 8QU*.",
     id: "0",
@@ -21,8 +21,8 @@ export const events = [
   {
     image: "/recycling_week.png",
     title: "Recycle Week 2023",
-    startDate: "17/02/2023",
-    endDate: "24/02/2023",
+    startDate: "2023-02-17",
+    endDate: "2023-02-24",
     description:
       "**What:** *Recycle Week* is set to be a major event in the calendar this year, coming hot on the heels of last year's campaign This year's theme is *\"Let's Get Real\"* and will challenge perceptions and myths around recycling, and target contamination to improve recycling behaviours.  \n&nbsp;  \n**How to get involved:** Our [partner pack for Recycle Week 2023](https://wrap.org.uk/resources/campaign-assets/recycle-week-2022-partner-pack), which gives information on the theme and ways to get involved, is available now to download and share with colleagues - so your organisation can begin planning its own campaign activity during the week.",
     id: "1",
@@ -30,8 +30,8 @@ export const events = [
   {
     image: "/brentford_recycling_action_group.png",
     title: "Brentford Recycling Action Group Monthly Meeting",
-    startDate: "07/03/2023",
-    endDate: "07/03/2023",
+    startDate: "2023-03-07",
+    endDate: "2023-03-07",
     description:
       "**What:** *BRAG (Brentford Recycling Action Group)* is a non-political community waste action group made up of local Brentford residents. We work to raise awareness of waste issues and to promote activities and facilities to improve the local environment for the benefit of local people. Meet our fun and enthusiastic group and get inspired!  \n&nbsp;  \n**When:** Our *monthly meetings* take place the *first Tuesday of the month*. The meeting *starts at 7pm* and *finishes at 8pm*.  \n&nbsp;  \n**Where:** At the *Residents Clubroom, Brentford Dock, 1 Augustus Close, Brentford, TW8 8QU*.",
     id: "2",
@@ -39,8 +39,8 @@ export const events = [
   {
     image: "/global_recycling_day.png",
     title: "Global Recycling Day",
-    startDate: "18/03/2023",
-    endDate: "18/03/2023",
+    startDate: "2023-03-18",
+    endDate: "2023-03-18",
     description:
       "**What:** *The Global Recycling Foundation* is pleased to announce the theme of *Global Recycling Day 2023* as *#RecyclingHeroes*. This will recognise the people, places and activities that showcase what an important role recycling plays in contributing to an environmentally stable planet and a greener future which will benefit all.  \n&nbsp;  \n**Competition:** We are inviting nominations for *#RecyclingHeroes*. Ten winners will be chosen from across the globe and each one will be awarded a prize of *US $1000* and will also feature on all our social media channels. *For more info on nominations*, [see here](https://www.globalrecyclingday.com/recycling-heroes/2023-competition/).",
     id: "3",
@@ -48,8 +48,8 @@ export const events = [
   {
     image: "/garden_waste_collection_service.png",
     title: "Garden Waste Collection Service - New Cycle",
-    startDate: "01/04/2023",
-    endDate: "01/04/2023",
+    startDate: "2023-04-01",
+    endDate: "2023-04-01",
     description:
       "**What:** *Hounslow Council's Garden Waste Collection Service* provides residents with *25 fortnightly collections from 1 April 2023 to 31 March 2024*, with a break of two weeks over the Christmas period. The service is a convenient, low-cost way for residents with gardens to dispose of cuttings throughout the year. Residents are advised to sign up *before 1 April* to ensure they receive the service in full. This will ensure that current subscribers don't experience any interruption to their service and new subscribers are set up ahead of their first collection on 1 April.  \n&nbsp;  \n**Cost:** The cost for the service for the year is *£65.00* or *£32.50* for those in receipt of *Council Tax Reduction* (support). If you're currently a Garden Waste subscriber and paid with direct debit last year, please be careful not to subscribe again as you may be due to renew automatically; look out for notifications about your subscription before renewing.",
     id: "4",
