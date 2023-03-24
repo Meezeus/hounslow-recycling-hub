1cece74d956d7ee89d9400a72195488a1ccfa37b
Meezeus <kac3131@gmail.com>
2023-03-24 14:08:14 +0000

    Add ids

--- data/Quiz.tsx
+++ data/Quiz.tsx
@@ -1,11 +1,13 @@
 export type Question = {
   question: string;
   answers: Array<Answer>;
+  id: string;
 };
 
 export type Answer = {
   answer: string;
   correct: boolean;
+  id: string;
 };
 
 export const quiz = [
@@ -15,20 +17,25 @@ export const quiz = [
       {
         answer: "Plastic",
         correct: true,
+        id: "a",
       },
       {
         answer: "Card",
         correct: false,
+        id: "b",
       },
       {
         answer: "Paper",
         correct: false,
+        id: "c",
       },
       {
         answer: "Glass",
         correct: false,
+        id: "d",
       },
     ],
+    id: "0",
   },
   {
     question: "What kind of item can you recycle using you red recycling box?",
@@ -36,20 +43,25 @@ export const quiz = [
       {
         answer: "Metal",
         correct: true,
+        id: "a",
       },
       {
         answer: "Card",
         correct: false,
+        id: "b",
       },
       {
         answer: "Paper",
         correct: false,
+        id: "c",
       },
       {
         answer: "Glass",
         correct: false,
+        id: "d",
       },
     ],
+    id: "1",
   },
   {
     question: "What kind of item can you recycle using you blue recycling box?",
@@ -57,20 +69,25 @@ export const quiz = [
       {
         answer: "Plastic",
         correct: false,
+        id: "a",
       },
       {
         answer: "Metal",
         correct: false,
+        id: "b",
       },
       {
         answer: "Card",
         correct: true,
+        id: "c",
       },
       {
         answer: "Glass",
         correct: false,
+        id: "d",
       },
     ],
+    id: "2",
   },
   {
     question: "What kind of item can you recycle using you blue recycling box?",
@@ -78,20 +95,25 @@ export const quiz = [
       {
         answer: "Plastic",
         correct: false,
+        id: "a",
       },
       {
         answer: "Metal",
         correct: false,
+        id: "b",
       },
       {
         answer: "Paper",
         correct: true,
+        id: "c",
       },
       {
         answer: "Glass",
         correct: false,
+        id: "d",
       },
     ],
+    id: "3",
   },
   {
     question:
@@ -100,20 +122,25 @@ export const quiz = [
       {
         answer: "Plastic",
         correct: false,
+        id: "a",
       },
       {
         answer: "Card",
         correct: false,
+        id: "b",
       },
       {
         answer: "Paper",
         correct: false,
+        id: "c",
       },
       {
         answer: "Glass",
         correct: true,
+        id: "d",
       },
     ],
+    id: "4",
   },
   {
     question: "What is the correct way to recycle plastic?",
@@ -121,20 +148,25 @@ export const quiz = [
       {
         answer: "Red recycling box",
         correct: true,
+        id: "a",
       },
       {
         answer: "Blue recycling box",
         correct: false,
+        id: "b",
       },
       {
         answer: "Green recycling box",
         correct: false,
+        id: "c",
       },
       {
         answer: "Black wheeled bin",
         correct: false,
+        id: "d",
       },
     ],
+    id: "5",
   },
   {
     question: "What is the correct way to recycle metal?",
@@ -142,20 +174,25 @@ export const quiz = [
       {
         answer: "Red recycling box",
         correct: true,
+        id: "a",
       },
       {
         answer: "Blue recycling box",
         correct: false,
+        id: "b",
       },
       {
         answer: "Green recycling box",
         correct: false,
+        id: "c",
       },
       {
         answer: "Black wheeled bin",
         correct: false,
+        id: "d",
       },
     ],
+    id: "6",
   },
   {
     question: "What is the correct way to recycle card?",
@@ -163,20 +200,25 @@ export const quiz = [
       {
         answer: "Red recycling box",
         correct: false,
+        id: "a",
       },
       {
         answer: "Blue Recycling box",
         correct: true,
+        id: "b",
       },
       {
         answer: "Green recycling box",
         correct: false,
+        id: "c",
       },
       {
         answer: "Black wheeled bin",
         correct: false,
+        id: "d",
       },
     ],
+    id: "7",
   },
   {
     question: "What is the correct way to recycle paper?",
@@ -184,20 +226,25 @@ export const quiz = [
       {
         answer: "Red recycling box",
         correct: false,
+        id: "a",
       },
       {
         answer: "Blue Recycling box",
         correct: true,
+        id: "b",
       },
       {
         answer: "Green recycling box",
         correct: false,
+        id: "c",
       },
       {
         answer: "Black wheeled bin",
         correct: false,
+        id: "d",
       },
     ],
+    id: "8",
   },
   {
     question: "What is the correct way to recycle glass?",
@@ -205,20 +252,25 @@ export const quiz = [
       {
         answer: "Red recycling box",
         correct: false,
+        id: "a",
       },
       {
         answer: "Blue Recycling box",
         correct: false,
+        id: "b",
       },
       {
         answer: "Green recycling box",
         correct: true,
+        id: "c",
       },
       {
         answer: "Black wheeled bin",
         correct: false,
+        id: "d",
       },
     ],
+    id: "9",
   },
   {
     question: "What is the correct way to recycle textiles and clothing?",
@@ -226,20 +278,25 @@ export const quiz = [
       {
         answer: "Red recycling box",
         correct: false,
+        id: "1a",
       },
       {
         answer: "Blue Recycling box",
         correct: false,
+        id: "1b",
       },
       {
         answer: "Green recycling box",
         correct: false,
+        id: "1c",
       },
       {
         answer: "Tied carrier bag",
         correct: true,
+        id: "1d",
       },
     ],
+    id: "10",
   },
   {
     question: "What is the correct way to recycle small electrical items?",
@@ -247,20 +304,25 @@ export const quiz = [
       {
         answer: "Red recycling box",
         correct: false,
+        id: "1a",
       },
       {
         answer: "Blue Recycling box",
         correct: false,
+        id: "1b",
       },
       {
         answer: "Green recycling box",
         correct: false,
+        id: "1c",
       },
       {
         answer: "Tied carrier bag",
         correct: true,
+        id: "1d",
       },
     ],
+    id: "11",
   },
   {
     question:
@@ -269,20 +331,25 @@ export const quiz = [
       {
         answer: "Plastic bottle",
         correct: false,
+        id: "1a",
       },
       {
         answer: "Drink can",
         correct: false,
+        id: "1b",
       },
       {
         answer: "Aluminium foil",
         correct: false,
+        id: "1c",
       },
       {
         answer: "Plastic wrapping and packaging",
         correct: true,
+        id: "1d",
       },
     ],
+    id: "12",
   },
   {
     question:
@@ -291,20 +358,25 @@ export const quiz = [
       {
         answer: "Cardboard",
         correct: false,
+        id: "1a",
       },
       {
         answer: "Newspapers",
         correct: false,
+        id: "1b",
       },
       {
         answer: "Tetra Paks",
         correct: false,
+        id: "1c",
       },
       {
         answer: "Shredded paper",
         correct: true,
+        id: "1d",
       },
     ],
+    id: "13",
   },
   {
     question:
@@ -313,20 +385,25 @@ export const quiz = [
       {
         answer: "Glass bottles",
         correct: false,
+        id: "1a",
       },
       {
         answer: "Glass jars",
         correct: false,
+        id: "1b",
       },
       {
         answer: "Glass",
         correct: false,
+        id: "1c",
       },
       {
         answer: "Ceramics",
         correct: true,
+        id: "1d",
       },
     ],
+    id: "14",
   },
   {
     question:
@@ -335,20 +412,25 @@ export const quiz = [
       {
         answer: "Fruit and vegetable peelings",
         correct: false,
+        id: "1a",
       },
       {
         answer: "Tea bags and coffee granules",
         correct: false,
+        id: "1b",
       },
       {
         answer: "Out of date meat and fish",
         correct: false,
+        id: "1c",
       },
       {
         answer: "Liquids and oils",
         correct: true,
+        id: "1d",
       },
     ],
+    id: "15",
   },
   {
     question:
@@ -357,19 +439,24 @@ export const quiz = [
       {
         answer: "Grass cuttings",
         correct: false,
+        id: "1a",
       },
       {
         answer: "Leaves",
         correct: false,
+        id: "1b",
       },
       {
         answer: "Twigs and small branches",
         correct: false,
+        id: "1c",
       },
       {
         answer: "Soil",
         correct: true,
+        id: "1d",
       },
     ],
+    id: "16",
   },
 ];
