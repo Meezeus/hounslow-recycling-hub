45f4a1e8553468fabab27304648fbabe8b9e26d1
Meezeus <kac3131@gmail.com>
2023-12-18 14:07:24 +0000

    Remove unused code

--- components/home/Subheading.tsx
+++ components/home/Subheading.tsx
@@ -10,22 +10,16 @@ type SubheadingProps = {
 
 export default React.forwardRef<HTMLDivElement, SubheadingProps>(
   function Subheading(props, ref) {
-    const [isHovered, setIsHovered] = useState(false);
-
     return (
       <div
         ref={ref}
         className={style["subheading-container-wrapper"]}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
       >
         <a className={homeStyle["anchor"]} id={props.id + "-anchor"}></a>
         <div className={style["subheading-container"]}>
           <div className={style["subheading-anchors"]}></div>
           <h3
-            className={`${style["subheading-text"]} ${
-              isHovered ? style["subheading-text-hover"] : ""
-            }`}
+            className={`${style["subheading-text"]}`}
           >
             {props.title}
           </h3>
