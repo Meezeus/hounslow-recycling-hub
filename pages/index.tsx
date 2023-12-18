ea92d988d2a7c7d7972149dee46cab9b8866e370
Meezeus <kac3131@gmail.com>
2023-12-18 13:17:59 +0000

    Remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -263,48 +263,6 @@ export default function Home({ data }: Props) {
 }
 
 export const getServerSideProps = async () => {
-  // const headers = {
-  //   "content-type": "application/json",
-  //   "x-api-key": `${process.env.FRONTEND_APIKEY}`,
-  // };
-
-  // const cats = [
-  //   "facts",
-  //   "quiz",
-  //   "events",
-  //   "recyclingServices",
-  //   "dumpedRubbishInfo",
-  // ];
-  // const data = Object.fromEntries(cats.map((cat) => [cat, ""]));
-
-  // for (let i = 0; i < cats.length; i++) {
-  //   const resapi = await fetch(
-  //     `${process.env.NEXT_PUBLIC_BASEURL}/api/${cats[i]}`,
-  //     {
-  //       method: "GET",
-  //       headers: headers,
-  //     }
-  //   );
-  //   data[cats[i]] = await resapi.json();
-  // }
-
-  // return {
-  //   props: {
-  //     data: {
-  //       facts: Object.keys(data.facts).length !== 0 ? data.facts : [],
-  //       quiz: Object.keys(data.quiz).length !== 0 ? data.quiz : [],
-  //       events: Object.keys(data.events).length !== 0 ? data.events : [],
-  //       recyclingServices:
-  //         Object.keys(data.recyclingServices).length !== 0
-  //           ? data.recyclingServices
-  //           : [],
-  //       dumpedRubbishInfo:
-  //         Object.keys(data.dumpedRubbishInfo).length !== 0
-  //           ? data.dumpedRubbishInfo[0]
-  //           : [],
-  //     },
-  //   },
-  // };
   return {
     props: {
       data: {
