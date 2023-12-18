3d777df85e07ed9d4315ac65487bbf58a59e50af
Meezeus <kac3131@gmail.com>
2023-12-18 12:03:35 +0000

    Remove unused styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,6 @@
 import "@/styles/globals.css";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import "animate.css";
 import "@/styles/style.css";
-import "@aws-amplify/ui-react/styles.css";
-import "@/styles/ampl.css";
+import "animate.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
